5fb3737a0f4afbb3e14a229141bc906df7f4c4c7
Naveen <nknaveen379@gmail.com>
2020-04-06 19:25:09 +0530

    few new files and changes

--- user/views.py
+++ user/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from . forms import UserRegisterForm
-
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -10,8 +10,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f' Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f' Account has been created for you !! Now you can log in.')
+            return redirect('login')
 
     form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+@login_required 
+def profile(request):
+    return render(request, 'user/profile.html')
