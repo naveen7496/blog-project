a07a33df8977826a51b7d77d3d7deffdc25d66f3
Naveen <nknaveen379@gmail.com>
2020-04-10 00:57:34 +0530

    few more files

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,9 @@ from django.http import HttpResponse
 from .models import Post
 
 
-
 def home(request):
     posts = Post.objects.all()
     print(type(posts))
     print(posts, '#######################################################')
     context = {'posts': posts}
     return render(request, 'blog/home.html', context)
-
-
-
--- django_project/urls.py
+++ django_project/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from user import views as user_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,4 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('profile/', user_view.profile, name='profile'),
     path('blog/', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
